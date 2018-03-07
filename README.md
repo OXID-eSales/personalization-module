df7baef7b886b1a983fe24e4f782b0954d076b1d
vilmal <vilma.liorensaityte@gmail.com>
2018-03-07 10:51:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Econda module
+=============
+
+Module adds Econda service functionality.
+
+## Requirements
+
+* OXID eShop compilation ^v6.1.0
+
+## Installation
+
+* Register module repository in project root `composer.json` file.
+* Execute command: `composer require oxid-esales/econda-module:^1.0.0`.
+
+## Features
+
+Module provides functionality which allows:
+* Add widgets
+* ...
+
+## License
+
+See LICENSE file for license details.
---
+++ metadata.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.0';
+
+/**
+ * Module information.
+ */
+$aModule = array(
+    'id'           => 'oeeconda',
+    'title'        => 'Econda',
+    'description'  => [
+        'de' => 'Modul Econda',
+        'en' => 'Module Econda',
+    ],
+    'thumbnail' => '/out/pictures/logo.png',
+    'version' => '1.0.0',
+    'author' => 'OXID eSales AG',
+    'url' => 'http://www.oxid-esales.com',
+    'email' => 'info@oxid-esales.com',
+    'extend' => [],
+    'controllers' => [],
+    'events' => [],
+    'templates' => [],
+    'blocks' => [],
+    'settings' => []
+);
