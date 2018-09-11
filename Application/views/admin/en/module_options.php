5f10eb3ccbf7b46ab9e3e29b37f7cde686358a48
Jürgen Busch <juergen.jb.busch@gmail.com>
2018-09-11 15:29:36 +0200

    Small improvements in language files DE|EN.

--- Application/views/admin/en/module_options.php
+++ Application/views/admin/en/module_options.php
@@ -7,37 +7,37 @@
 $aLang = [
     'charset' => 'UTF-8',
 
-    'SHOP_MODULE_GROUP_oepersonalization_account' => 'Econda account',
-    'SHOP_MODULE_sOePersonalizationAccountId' => 'Account Id',
-    'SHOP_MODULE_blOePersonalizationEnableWidgets' => 'Enable Econda widgets',
-    'SHOP_MODULE_blOePersonalizationUseDemoAccount' => 'Use Econda demo account',
+    'SHOP_MODULE_GROUP_oepersonalization_account' => 'econda account',
+    'SHOP_MODULE_sOePersonalizationAccountId' => 'ID of the econda account',
+    'SHOP_MODULE_blOePersonalizationEnableWidgets' => 'Enable econda recommandation widgets',
+    'SHOP_MODULE_blOePersonalizationUseDemoAccount' => 'Use products from the econda demo shop',
 
-    'SHOP_MODULE_GROUP_oepersonalization_start_page_widgets' => 'Start page widgets',
-    'SHOP_MODULE_sOePersonalizationWidgetIdStartPageBargainArticles'=> '<b>Weeks special</b> - widget Id',
-    'SHOP_MODULE_sOePersonalizationWidgetTemplateStartPageBargainArticles' => '<b>Weeks special</b> - widget template',
-    'SHOP_MODULE_sOePersonalizationWidgetIdStartPageTopArticles' => '<b>Top products</b> - widget Id',
-    'SHOP_MODULE_sOePersonalizationWidgetTemplateStartPageTopArticles' => '<b>Top products</b> - widget template',
+    'SHOP_MODULE_GROUP_oepersonalization_start_page_widgets' => 'Start page',
+    'SHOP_MODULE_sOePersonalizationWidgetIdStartPageBargainArticles'=> '<b>Week\'s special</b> - Widget ID',
+    'SHOP_MODULE_sOePersonalizationWidgetTemplateStartPageBargainArticles' => '<b>Week\'s special</b> - Widget template',
+    'SHOP_MODULE_sOePersonalizationWidgetIdStartPageTopArticles' => '<b>Top seller</b> - Widget ID',
+    'SHOP_MODULE_sOePersonalizationWidgetTemplateStartPageTopArticles' => '<b>Top seller</b> - Widget template',
 
-    'SHOP_MODULE_GROUP_oepersonalization_list_page_widgets' => 'List page widget',
-    'SHOP_MODULE_sOePersonalizationWidgetIdListPage' => 'Widget Id',
+    'SHOP_MODULE_GROUP_oepersonalization_list_page_widgets' => 'Category view',
+    'SHOP_MODULE_sOePersonalizationWidgetIdListPage' => 'Widget ID',
     'SHOP_MODULE_sOePersonalizationWidgetTemplateListPage' => 'Widget template',
 
-    'SHOP_MODULE_GROUP_oepersonalization_details_page_widgets' => 'Details page widget',
-    'SHOP_MODULE_sOePersonalizationWidgetIdDetailsPage' => 'Widget Id',
+    'SHOP_MODULE_GROUP_oepersonalization_details_page_widgets' => 'Details page',
+    'SHOP_MODULE_sOePersonalizationWidgetIdDetailsPage' => 'Widget ID',
     'SHOP_MODULE_sOePersonalizationWidgetTemplateDetailsPage' => 'Widget template',
 
-    'SHOP_MODULE_GROUP_oepersonalization_thank_you_page_widgets' => 'Thank you page widget',
-    'SHOP_MODULE_sOePersonalizationWidgetIdThankYouPage'=> 'Widget Id',
+    'SHOP_MODULE_GROUP_oepersonalization_thank_you_page_widgets' => 'Thank you page',
+    'SHOP_MODULE_sOePersonalizationWidgetIdThankYouPage'=> 'Widget ID',
     'SHOP_MODULE_sOePersonalizationWidgetTemplateThankYouPage' => 'Widget template',
 
-    'SHOP_MODULE_blOePersonalizationEnableTracking' => 'Enable Econda Analytics',
+    'SHOP_MODULE_blOePersonalizationEnableTracking' => 'Enable econda Analytics',
 
     'SHOP_MODULE_blOePersonalizationEnableTagManager' => 'Enable Tag Manager',
 
-    'SHOP_MODULE_sOePersonalizationTrackingShowNote' => 'Tracking Note',
+    'SHOP_MODULE_sOePersonalizationTrackingShowNote' => 'Note',
     'SHOP_MODULE_sOePersonalizationTrackingShowNoteNo' => 'Do not show',
-    'SHOP_MODULE_sOePersonalizationTrackingShowNoteOptIn' => 'Show "Allow tracking"',
-    'SHOP_MODULE_sOePersonalizationTrackingShowNoteOptOut' => 'Show "Deny tracking"',
+    'SHOP_MODULE_sOePersonalizationTrackingShowNoteOptIn' => 'Show "I agree (activate personalization)"',
+    'SHOP_MODULE_sOePersonalizationTrackingShowNoteOptOut' => 'Show "I disagree (deactivate personalization)"',
 
     'SHOP_MODULE_GROUP_oepersonalization_export' => 'Export',
     'SHOP_MODULE_sOePersonalizationExportPath'=> 'Export path',
