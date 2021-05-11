34877a48238871b6c995916593d2711d5d14d4e3
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2021-05-11 14:11:20 +0300

    Update admin credentials for CC tests

--- Tests/Codeception/_data/fixtures.php
+++ Tests/Codeception/_data/fixtures.php
@@ -1,14 +1,17 @@
 <?php
+
 /**
  * Copyright © OXID eSales AG. All rights reserved.
  * See LICENSE file for license details.
  */
+
 return [
     'adminUser' => [
-        "userId" => "admin",
-        "userLoginName" => "admin",
-        "userPassword" => "admin",
-        "userName" => "John",
-        "userLastName" => "Doe",
+        'userLoginName' => 'admin',
+        'userPassword' => 'admin',
+    ],
+    'adminUserEE' => [
+        'userLoginName' => 'admin@myoxideshop.com',
+        'userPassword' => 'admin0303',
     ],
 ];
